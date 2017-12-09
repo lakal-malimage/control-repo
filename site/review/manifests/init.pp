3bd0245abc8ca71f6375fc7c0c8ef780216f79c2
Lakal Malimage <lakal.malimage@gmail.com>
2017-12-09 13:13:56 +0530

    Update init.pp

--- site/review/manifests/init.pp
+++ site/review/manifests/init.pp
@@ -1,1 +1,17 @@
-
+class review (
+    $user = String undef,
+) {
+    $homedir = $user ? {
+      'root' => '/root',
+      defautl => "/home/${user}",
+    }
+    
+    user { "$user": 
+        ensure    => 'present',
+        comment   => 'Test account',
+        home      => "$homedir",
+        password  => '$1$TaBlusm3$xCAUdPp.Qbo4EPOqauuY/1',
+    }
+}
+   
+    
