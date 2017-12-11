aa75e4780b8d0b922cbf374551d0dba9b3bb619d
Lakal Malimage <lakal.malimage@gmail.com>
2017-12-11 06:25:10 +0530

    Update init.pp

--- site/review/manifests/init.pp
+++ site/review/manifests/init.pp
@@ -25,7 +25,7 @@ class review (
         content => epp( 'review/templ' , { 'name' => 'amal', 'arr' => ['sv1','sv2','sv3'] } ),
         }
         
-  include func
+  include review::func
   notice("END")
  }
    
