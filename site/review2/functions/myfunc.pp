d22e240336d2619b2c204dcbc870ef1ef93cb998
Lakal Malimage <lakal.malimage@gmail.com>
2017-12-13 07:34:44 +0530

    Update myfunc.pp

--- site/review2/functions/myfunc.pp
+++ site/review2/functions/myfunc.pp
@@ -1,7 +1,18 @@
-function puppet::myfunc (
-  Integer num1,
-  Integer num2,
-) {
-  $t = ($num1 + $num2)
+function puppet::myfunc( Variant[String,Numeric,Boolean] $inputvalue ) {
+  case $inputvalue {
+    Undef:   { false }
+    Boolean: { $inputvalue }
+    Numeric: {
+      case $inputvalue {
+        0:       { false }
+        default: { true }
+      }
+    }
+    String: {
+      case $inputvalue {
+        /^(?i:off|false|no|n|'')$/: { false }
+        default: { true }
+      }
+    }
   }
-
+}
