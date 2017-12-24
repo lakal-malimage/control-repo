d78fd0c9442894f48a66b56c0644e963f8fbf37f
Lakal Malimage <lakal.malimage@gmail.com>
2017-12-24 11:54:58 +0530

    Update lak.pp

--- site/review2/functions/lak.pp
+++ site/review2/functions/lak.pp
@@ -1,18 +1,3 @@
-function review2::lak( Variant[String,Numeric,Boolean] $inputvalue ) {
-  case $inputvalue {
-    Undef:   { false }
-    Boolean: { $inputvalue }
-    Numeric: {
-      case $inputvalue {
-        0:       { false }
-        default: { true }
-      }
-    }
-    String: {
-      case $inputvalue {
-        /^(?i:off|false|no|n|'')$/: { false }
-        default: { true }
-      }
-    }
-  }
+function review2::lak() {
+  notify { 'my functoin call': }
 }
